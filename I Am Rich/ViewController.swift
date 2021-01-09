da278aa5a09799f4901e4ed6cf4e7378e9e772c9
Larissa Marães <larissa.maraes@gmail.com>
2021-01-09 14:33:13 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Larissa Maraes on 09/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
